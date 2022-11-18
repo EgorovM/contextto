51e986cfc4a5f42dc97246d427beebc5001d1b8d
Michil Egorov <michilegorov@Michils-MacBook-Pro.local>
2022-11-18 16:00:20 +0300

    [feat] dont save matrix

--- words/guess.py
+++ words/guess.py
@@ -1,8 +1,6 @@
 from navec import Navec
-import numpy as np
-
 from django.conf import settings
-from sklearn.metrics.pairwise import cosine_distances
+from scipy.spatial.distance import cosine
 
 
 class Singleton(type):
@@ -19,11 +17,7 @@ class WordGuesser(metaclass=Singleton):
         self._navec = Navec.load(settings.BASE_DIR / 'models/navec_hudlit_v1_12B_500K_300d_100q.tar')
         self._word = word
         self._words = set(self._navec.vocab.words)
-        distances = cosine_distances(np.array([self._navec[self._word]]), np.array([
-            self._navec[word]
-            for word in self._words
-        ]))[0]
-
+        distances = [cosine(self._navec[self._word], self._navec[word]) for word in self._words]
         self._word_distances = sorted(zip(distances, self._words))
         self._word2order = {word: i for i, (_, word) in enumerate(self._word_distances)}
 
