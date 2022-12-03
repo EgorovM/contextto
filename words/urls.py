734d5a579b3f725b4666282d5969b9416a4705e1
NikitaSemenov <nikitasemenov0388@gmail.com>
2022-12-03 03:57:38 +0200

    [feat] add guess api

--- words/urls.py
+++ words/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import index, guess, clear_history, get_history, make_guess
 
-from .views import SessionView
+from .views import SessionView, GuessView
 from .routers import SimpleRouter
 
 router = SimpleRouter()
@@ -15,4 +15,6 @@ urlpatterns = [
     path('get_history/', get_history),
     path('make_guess/', make_guess),
     path('api/', include(router.urls)),
+    path('api/session/<str:id>/guess', GuessView.as_view({'get': 'list', 'post': 'create'}))
 ]
+
