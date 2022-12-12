b849251f9ee0c20babfd680b3705118dcff18ed8
EgorovM <egorov_michil@mail.ru>
2022-12-12 20:00:18 +0300

    [feat] add history view modal

--- words/urls.py
+++ words/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from words.views import index, guess, clear_history, get_history, make_guess
+from words.views import index, guess, clear_history, get_history, make_guess, get_attempts
 
 urlpatterns = [
     path('', index),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('clear_history/', clear_history),
     path('get_history/', get_history),
     path('make_guess/', make_guess),
+    path('get_attempts/', get_attempts),
 ]
